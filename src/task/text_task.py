c1edf09c657b5ccd4bdcb9d82aaa62825038df0a
theobayard <theobayarddevolo@gmail.com>
2023-12-29 17:35:35 -0700

    Convert to relative imports - streamlit doesn't like my src ones - I don't feel like making this into a proper module

--- src/task/text_task.py
+++ src/task/text_task.py
@@ -1,5 +1,5 @@
 import json
-from src.models import CHAT_DEFAULT
+from ..models import CHAT_DEFAULT
 
     
 def parse_chat_gpt_json(hopefully_valid_json_string, model = CHAT_DEFAULT, max_num_tries = 3):
